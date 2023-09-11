980718b3133bfbcd0ba4e0d1ab863ddf87afd516
Ritapossible <egwuaturita@gmail.com>
2023-09-11 08:23:47 -0700

    Header file updated

---
+++ x-shell.h
@@ -0,0 +1,234 @@
+#ifndef _X-SHELL_H_
+#define _X-SHELL_H_
+
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+#include <limits.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <fcntl.h>
+
+/* The below prototype points to an array of pointers to strings named the "environment" */
+
+extern char **enviro;
+
+/**
+ * struct data - The structure which contains all relevant data on the runtime
+ * @argv: Argument vector
+ * @input: The ommand line written by the user.
+ * @args: The tokens of the command line.
+ * @status: The last status of the x-shell.
+ * @counter_x: The lines counter.
+ * @_enviro: The environment variable.
+ * @pid: The process ID of the x-shell.
+ */
+
+typedef struct data
+{
+	char **argv;
+	char *input;
+	char **args;
+	int status;
+	int counter_x;
+	char **_enviro;
+	char *pid;
+} data_shell
+
+/**
+ * struct sep_list_s - Single linked list.
+ * @separator_x: ; | &
+ * @next: Next node of x-shell.
+ * Description_x: Single linked list to the store separators
+ */
+
+typedef struct sep_list_s
+{
+	char separator_x;
+	struct sep_list_s *next;
+} sep_list;
+
+/**
+ * struct line_list_s - Single linked list.
+ * @line_x: Command line.
+ * @next: Next node of x-shell.
+ * Description_x: Single linked list to the store command lines.
+ */
+
+typedef struct line_list_s
+{
+	char *line_x;
+	struct line_list_s *next;
+} line_list;
+
+/**
+ * struct r_var_list - Single linked list.
+ * @len_xvar: Length of the variable.
+ * @val_x: Value of the variable.
+ * @len_xval: Length of the value.
+ * @next: Next node.
+ * Description_x: Single linked list to the store variables.
+ */
+
+typedef struct r_var_list
+{
+	int len_xvar;
+	char *val_x;
+	int len_xval;
+	struct r_var_list *next;
+} r_var;
+
+/**
+ * struct builtin_s - Builtin structure for the command args.
+ * @name_x: The name of the command builtin i.e env, cd, exit etc.
+ * @func_x: The data type for pointer function.
+ */
+
+typedef struct builtin_s
+{
+	char *name_x;
+	int (*func_x)(data_shell *datash);
+} builtin_t;
+
+/* x_lists.c */
+line_list *add_line_node_end(line_list **head, char *line_x);
+void free_line_list(line_list **head);
+sep_list *add_sep_node_end(sep_list **head, char sep_x);
+void free_sep_list(sep_list **head);
+
+/* x_lists2.c */
+void free_rvar_list(r_var **head);
+r_var *add_rvar_node(r_var **head, int 1xvar, char *xvar, int lxval);
+
+/* x_str functions prototype */
+char *x_strcat(char *dest_x, const char *src_x);
+char *x_strcpy(char *dest_x, char *src_x);
+int x_strcmp(char *s1_x, char *s2_x);
+char *x_strchr(char *s_x, char c_x);
+int x_strspn(char *s_x, char *accept_x);
+
+/* x_mem.c */
+void *x_realloc(void *ptr_x, unsigned int x-old_size, unsigned int x-new_size);
+char **x_reallocdp(char **ptr_x, unsigned int x-old_size, unsigned int x-new_size);
+void x_memcpy(void *x-newptr, const void *ptr_x, unsigned int x_size);
+
+/* x_str2.c */
+int x-cmp_chars(char str[], const char *delim_x);
+char *x_strtok(char str[], const char *delim_x);
+int x_isdigit(const char *str_x);
+char *x_strdup(const char *str_x);
+int x_strlen(const char *str_x);
+
+/* x_str3.c */
+void rev_string(char *str_x);
+
+/* x-shell_loop.c */
+char *without_comment(char *in_x);
+void x-shell_loop(data_shell *datash);
+
+/* x-shell-check_syntax_error.c */
+int repeated_char(char *input_x, int i);
+int error_sep_op(char *input_x, int i, char last_x);
+int first_char(char *input_x, int *i);
+void print_syntax_error(data_shell *datash, char *input_x, int i, int bool_x);
+int check_syntax_error(data_shell *datash, char *input_x);
+
+/* read_x_line.c */
+char *read_line(int *x_eof);
+
+/* split_x.c */
+char *swap_char(char *input_x, int bool_x);
+void add_nodes(sep_list **head_x, line_list **head_l, char *input_x);
+void go_next(sep_list **list_x, line_list **list_l, data_shell *datash);
+int split_commands(data_shell *datash, char *input_x);
+char **split_line(char *input_x);
+
+/* rep_xvar.c */
+void check_env(r_var **head, char *in, data_shell *data);
+int check_vars(r_var **head, char *in, char *str, data_shell *data);
+char *replaced_input(r_var **head, char *input_x, char *xnew_input, int xlen);
+char *rep_var(char *input_x, data_shell *datash);
+
+/* x-get_line.c */
+void bring_line(char **lineptr_x, size_t *i, char *buffer, size_t j);
+ssize_t get_line(char **lineptr_x, size_t *i, FILE *stream);
+
+/* exec-x_line */
+int exec_line(data_shell *datash);
+
+/* cmd_exec_x.c */
+int is_cdir(char *x-path, int *i);
+char *_which(char *cmd_x, char **_enviro);
+int is_executable(data_shell *datash);
+int check_error_cmd(char *x_dir, data_shell *datash);
+int cmd_exec(data_shell *datash);
+
+/* x_env1.c */
+char *_getenv(const char *name, char **_enviro);
+int _env(data_shell *datash);
+
+/* x_env2.c */
+char *copy_info(char *name, char *x-value);
+void set_env(char *name, char *x-value, data_shell *datash);
+int _setenv(data_shell *datash);
+int _unsetenv(data_shell *datash);
+
+/* cd_x.c */
+void cd_dot(data_shell *datash);
+void cd_to(data_shell *datash);
+void cd_previous(data_shell *datash);
+void cd_to_home(data_shell *datash);
+
+/* cd_x_shell.c */
+int cd_x_shell(data_shell *datash);
+
+/* get_x_builtin */
+int (*get_x_builtin(char *cmd-x))(data_shell *datash);
+
+/* x_exit.c */
+int exit_x_shell(data_shell *datash);
+
+/* x_stdlib.c */
+int get_x_len(int i);
+char *x_itoa(int i);
+int _atoi(char *x);
+
+/* x_error1.c */
+char *strcat_cd(data_shell *, char *, char *, char *);
+char *error_get_cd(data_shell *datash);
+char *error_not_found(data_shell *datash);
+char *error_exit_x_shell(data_shell *datash);
+
+/* x_error2.c */
+char *error_get_alias(char **args);
+char *error_env(data_shell *datash);
+char *error_syntax(char **args);
+char *error_permission(char **args);
+char *error_path_126(data_shell *datash);
+
+/* get_x_error.c */
+int get_x_error(data_shell *datash, int e-x-val);
+
+/* get_x_sigint.c */
+void get_x_sigint(int x_sig);
+
+/* x_help1.c */
+void x_help_env(void);
+void x_help_setenv(void);
+void x_help_unsetenv(void);
+void x_help_general(void);
+void x_help_exit(void);
+
+/* x_help2.c */
+void x_help(void);
+void x_help_alias(void);
+void x_help_cd(void);
+
+/* get_x_help.c */
+int get_x_help(data_shell *datash);
+
+#endif
+
