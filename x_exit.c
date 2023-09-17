8ae84a66b02f2e05bdd7c896d799a082d2bbb5a0
Ritapossible <egwuaturita@gmail.com>
2023-09-17 03:55:09 -0700

    Exit file updated

--- x_exit.c
+++ x_exit.c
@@ -9,24 +9,24 @@
 
 int exit_x_shell(data_shell *datash)
 {
-	unsigned int x_status;
+	unsigned int status;
 	int x_digit;
 	int x_str_len;
 	int large_number;
 
 	if (datash->args[1] != NULL)
 	{
-		x_status = _atoi(datash->args[1]);
+		status = _atoi(datash->args[1]);
 		x_digit = _xdigit(datash->args[1]);
 		x_str_len = _xstrlen(datash->args[1]);
-		large_number = x_status > (unsigned int)INT_MAX;
+		large_number = status > (unsigned int)INT_MAX;
 		if (!x_digit || x_str_len > 10 || large_number)
 		{
 			get_x_error(datash, 2);
-			datash->x_status = 2;
+			datash->status = 2;
 			return (1);
 		}
-		datash->x_status = (x_status % 256);
+		datash->status = (status % 256);
 	}
 	return (0);
 }
