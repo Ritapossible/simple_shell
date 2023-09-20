12aff3f66ea04ff6198405e83326ea9bbeae5ca4
jackmarley254 <engndiritu1@gmail.com>
2023-09-20 22:39:08 +0300

    Debugging in shell

--- execute.c
+++ execute.c
@@ -10,12 +10,12 @@ int execute(my_program_info *data)
 	pid_t pidd;
 
 	/* check for program in built ins */
-	retval = builtins_list(data); 
+	retval = buitins_list(data);
 	if (retval != -1)/* if program was found in built ins */
 		return (retval);
 
 	/* check for program file system */
-	retval = find_program(data); 
+	retval = find_program(data);
 	if (retval)
 	{/* if program not found */
 		return (retval);
